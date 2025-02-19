a66cf5c73b1bceab9e45f0dabf1b5fe4b0d6ddd6
Jack1812 <jackandypabloalvarado@gmail.com>
2025-02-19 11:24:17 -0500

    2 parte

---
+++ app.js
@@ -0,0 +1,12 @@
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector (elemento);
+    elementoHTML.innerHTML = texto;
+
+}
+
+function intentoDeUsuaruio(){
+    alert("Click desde el boton");
+}
+
+asignarTextoElemento('h1','Juego del numero secreto');
+asignarTextoElemento('p','Indica un numero del 1 al 10');
