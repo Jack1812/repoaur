2f4355885c9351db8620a416e89ea4fda76536bb
Jack1812 <jackandypabloalvarado@gmail.com>
2025-02-20 12:28:05 -0500

    BOTON NUEVO

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 let numeroSecreto = generarNumeroSecreto();
 let intentos = 1;
 
-
 function asignarTextoElemento(elemento, texto){
-    let elementoHTML = document.querySelector (elemento);
+    let elementoHTML = document.querySelector(elemento);
     elementoHTML.innerHTML = texto;
     return;
 }
@@ -13,22 +12,27 @@ function verificarIntento(){
     console.log(intentos);
     if (numeroDeUsuario === numeroSecreto){
         asignarTextoElemento('p',`Felicidades! Adivinaste el numero secreto en ${intentos} ${intentos === 1 ? 'intento' : 'intentos'}`);
-
-    }else {
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        // el usuario no adivino el numero secreto
         if (numeroDeUsuario > numeroSecreto) {
             asignarTextoElemento('p','El numero secreto es menor');
+        
         } else {
             asignarTextoElemento('p','El numero secreto es mayor');
-         }
-            intentos++;
-
+        }
+        intentos++;
+        limpiarCaja();
     }
     return;
 }
 
+function limpiarCaja(){
+    document.querySelector('#valorUsuario').value = '';
+}
+
 function generarNumeroSecreto(){
-    return Math.floor(Math.random()*10)+1;
-   
+    return Math.floor(Math.random() * 10) + 1;
 }
 
 asignarTextoElemento('h1','Juego del numero secreto');
